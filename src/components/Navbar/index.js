e400598f163967c00085e59e85b9ed9547271d11
dranyl <dranyl@outlook.com>
2021-11-23 01:25:53 +1300

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <h1>Draynl</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contacts' activeStyle>
+            Contacts
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
